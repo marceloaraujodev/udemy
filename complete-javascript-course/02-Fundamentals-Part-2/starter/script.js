83407355e132d48017dcbcf34d4c5596c2354164
codetrsh <134286416+codetrsh@users.noreply.github.com>
2023-06-11 21:34:38 -0300

    finish fundamentals 2

--- complete-javascript-course/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course/02-Fundamentals-Part-2/starter/script.js
@@ -374,10 +374,53 @@ const jonas = [
 
 
 // // WHILE LOOP--------------------------
-let rep = 1
+// let rep = 1
 
-while (rep <= 10){
-    console.log(`WHILE: Lifting weight repetition ${rep}`)
-    rep++
+// while (rep <= 10){
+//     console.log(`WHILE: Lifting weight repetition ${rep}`)
+//     rep++
+// }
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+
+// while (dice !== 6){
+// console.log(`You rolled a ${dice}`)
+// dice = Math.trunc(Math.random() * 6) + 1;
+//     if(dice === 6){
+//         console.log('You rolled a',dice, "the loop is about to end")
+//     }
+// }
+
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;   
+  }
+
+  for(let i = 0; i < bills.length; i++){
+    const tip = calcTip(bills[i])
+    tips.push(tip)
+    totals.push(tip + bills[i])
+}
+// console.log('Bills: ', bills, 'Tips: ', tips, 'Totals: ', totals)
+console.log('Tips: ', tips)
+  
+
+
+function calcAverage(arr){
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        // sum = sum + arr[i] //the bottom is the same!
+        sum += arr[i]
+    }
+    // console.log('Sum: ', sum)
+    // console.log('Average: ', sum / arr.length)
+    return sum / arr.length
 }
 
+// console.log(calcAverage([2,3,7]))
+console.log('Average of Bills: ', calcAverage(totals))
+console.log('Average of tips: ', calcAverage(tips))
