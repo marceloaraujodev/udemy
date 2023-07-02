21cac99b40f34b09a6ea4ca876010513ccd5a31a
codetrsh <134286416+codetrsh@users.noreply.github.com>
2023-07-02 00:37:34 -0300

    ngt

--- complete-javascript-course/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -21,9 +21,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-06-28T17:01:17.194Z',
+    '2023-06-30T23:36:17.929Z',
+    '2023-07-01T23:08:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -41,7 +41,7 @@ const account2 = {
     '2019-12-25T06:04:23.907Z',
     '2020-01-25T14:18:46.235Z',
     '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
+    '2020-06-10T14:43:26.374Z',
     '2020-06-25T18:49:59.371Z',
     '2020-07-26T12:01:20.894Z',
   ],
@@ -79,22 +79,55 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+// FORMATING DATES
+const formatMovementsDate = (date, locale) => {
+  const calcdaysPassed = (date1, date2) => Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24))
+
+  const daysPassed = calcdaysPassed (new Date(), date)
+
+  if(daysPassed === 0) return 'Today'
+  if(daysPassed === 1) return 'Yesterday'
+  if(daysPassed <= 7) return `${daysPassed} days ago`
+
+  const day = `${date.getDate()}`.padStart(2,'0')
+  const month = `${date.getMonth() + 1}`.padStart(2, 0)
+  const year = date.getFullYear()
+  
+  return new Intl.DateTimeFormat(locale).format(date)
+}
+
+
+// FORMATING CURRENCY
+const formatCur = (value, locale, currency) => {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency
+  }).format(value)
+}
+
+
 // Individual movements in the statement
 // sort set to false by default in the parameter
-const displayMovements = function(movements, sort = false){
+const displayMovements = function(acc, sort = false){
   containerMovements.innerHTML = '';
 
   // wanna copy the movements array to sort it since the sort method changes the array
-  const movs = sort ? movements.slice().sort((a,b) => a - b) : movements
+  const movs = sort ? acc.movements.slice().sort((a,b) => a - b) : acc.movements
 
   movs.forEach((mov, i) => {
   const type = mov > 0 ? 'deposit' : 'withdrawal'
 
+  const date = new Date(acc.movementsDates[i])
+  const displayDate = formatMovementsDate(date, acc.locale)
+
+  const formattedMov = formatCur(mov, acc.locale, acc.currency)
+  
+
   const html = `  
   <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i +  1} ${type}</div>
-    <div class="movements__date">3 days ago</div>
-    <div class="movements__value">${mov}€</div>
+    <div class="movements__date">${displayDate}</div> 
+    <div class="movements__value">${formattedMov}</div>
   </div>`
 
   containerMovements.insertAdjacentHTML('afterbegin', html)
@@ -106,7 +139,7 @@ const displayMovements = function(movements, sort = false){
 // Display Balance 
 const calcDisplayBalance = function(acc){
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0)
-  labelBalance.textContent = `${acc.balance} €`
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency)
 }
 
 
@@ -115,12 +148,12 @@ const calcDisplaySummary = function (acc){
   const incomes = acc.movements
   .filter(mov => mov > 0)
   .reduce((acc, mov) => acc + mov, 0)
-  labelSumIn.textContent = `${incomes}€`
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency)
 
   const out = acc.movements
   .filter(mov => mov < 0)
   .reduce((acc, mov) => acc + mov, 0)
-  labelSumOut.textContent = `${Math.abs(out)} €` // Math.abs removes the negative sign 
+  labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency)
 
   const interest = acc.movements
   .filter(mov => mov > 0)
@@ -130,7 +163,7 @@ const calcDisplaySummary = function (acc){
     return int >= 1
   })
   .reduce((acc, interest) => acc += interest, 0)
-  labelSumInterest.textContent = `${interest} €`
+  labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency)
 }
 
 
@@ -151,7 +184,7 @@ createUserNames(accounts)
 
 function updateUI(acc){
       //Display movements
-      displayMovements(acc.movements)
+      displayMovements(acc)
       //Display balance
       calcDisplayBalance(acc)
       //Display summary
@@ -161,7 +194,28 @@ function updateUI(acc){
 
 //Event Handlers 
 let currentAccount;
+//----------Remove when finished
+// FAKE ALWAYS LOGGED IN - remove it to test logging in - leave it to work on interface
+currentAccount = account1
+updateUI(currentAccount)
+containerApp.style.opacity = 100
 
+//// Experimenting with API
+////----- Timbe below Current Balance
+// const now = new Date()
+// const options = {
+//  hour: 'numeric',
+//  minute: 'numeric',
+//  day: 'numeric',
+//  month: 'long',
+//  year: 'numeric',
+//  weekday: 'long'
+// }
+// // const locale = navigator.language
+// // can change the 'pt-BR' on the new Intl.DateTimeFormat as a parameter
+// labelDate.textContent = new Intl.DateTimeFormat('pt-BR', options).format(now)
+
+//--------------------------
 
 //// LOGIN AND DISPLAY ACCOUNTS
 // ? the optional chaining in the btnLogin, first checks if the current account exists, if it doesnt the rest of the code will not run. If it does exist it will go to the pin ...
@@ -171,10 +225,36 @@ btnLogin.addEventListener('click', (e)=>{
   currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value)
 
 
-  if(currentAccount?.pin === Number(inputLoginPin.value)){
+  if(currentAccount?.pin === +inputLoginPin.value){
     // display UI and Welcome msg
     labelWelcome.textContent = `Welcome Back ${currentAccount.owner.split(' ')[0]}`
     containerApp.style.opacity = '100'
+
+
+    // Create and displaying date - day/month/year
+    const now = new Date()
+    const options = {
+     hour: 'numeric',
+     minute: 'numeric',
+     day: 'numeric',
+     month: 'numeric',
+     year: 'numeric',
+    //  weekday: 'long'
+    }
+    const locale = navigator.language
+    // can change the 'pt-BR' on the new Intl.DateTimeFormat as a parameter
+    labelDate.textContent = new Intl.DateTimeFormat(currentAccount.locale, options).format(now)
+
+    /* Show compared to above how much simplier it got with the Intl.DateTimeFormat
+    // const now = new Date()
+    // const day = `${now.getDate()}`.padStart(2,'0')
+    // const month = `${now.getMonth() + 1}`.padStart(2, 0)
+    // const year = now.getFullYear()
+    // const hour = `${now.getHours()}`.padStart(2, 0)
+    // const min = `${now.getMinutes()}`.padStart(2, 0)
+    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`
+    */
+
 
     //clear input fields // assignment operator works from right to left
     inputLoginUsername.value = inputLoginPin.value = '';
@@ -186,31 +266,42 @@ btnLogin.addEventListener('click', (e)=>{
   }
 })
 
+
 //// The preventDefault is need so it wont reload the page! 
 btnTransfer.addEventListener('click', (e)=>{
   e.preventDefault()
-  const amount =  Number(inputTransferAmount.value)
+  const amount = +inputTransferAmount.value
   const receiverAcc = accounts.find(acc => acc.username === inputTransferTo.value) 
   inputTransferTo.value = inputTransferAmount.value = ''; // assignment goes right to left can 2 in 1!
 
   if(amount > 0 && receiverAcc && currentAccount.balance >= amount && receiverAcc?.username !== currentAccount.username){
+
     //doing the transfer
     currentAccount.movements.push(-amount)
     receiverAcc.movements.push(amount)
-    console.log('Transfer valid')
+
+    //Add transfer date
+    currentAccount.movementsDates.push(new Date().toISOString())
+    receiverAcc.movementsDates.push(new Date().toISOString())
+
+    // Update UI 
     updateUI(currentAccount)
   }
 
 })
 
-
+// LOAN
 btnLoan.addEventListener('click', (e)=>{
   e.preventDefault()
-  const amount = Number(inputLoanAmount.value)
+  const amount = Math.floor(inputLoanAmount.value)
 
   if(amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)){
     // add positive movement
     currentAccount.movements.push(amount)
+
+    // Loan Date
+      currentAccount.movementsDates.push(new Date().toISOString())
+
     // Update UI
     updateUI(currentAccount)
   }
@@ -223,7 +314,7 @@ btnClose.addEventListener('click', (e)=>{
   e.preventDefault()
   
   
-  if(inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin){
+  if(inputCloseUsername.value === currentAccount.username && +inputClosePin.value === currentAccount.pin){
     const index = accounts.findIndex(acc => acc.username === currentAccount.username)
     // Delete Account
     accounts.splice(index, 1)
@@ -239,7 +330,7 @@ let sorted = false
 // activates the sorted button
 btnSort.addEventListener('click', (e)=>{
   e.preventDefault()
-  displayMovements(currentAccount.movements, !sorted)
+  displayMovements(currentAccount, !sorted)
   sorted = !sorted
 })
 
@@ -250,3 +341,133 @@ btnSort.addEventListener('click', (e)=>{
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+
+//// Parsing
+
+//// Parse accepts 2 parameters the second being radix its the base of the numeral system we are using, We should pass 10 most of the time. Binary system would be something else. 
+//// String needs to start with a number to be converted correctly
+
+// console.log(Number.parseInt('30px', 10)) 
+// console.log(Number.parseInt('e23', 10))  
+
+
+// console.log(Number.parseInt(' 2.5rem'))  
+// console.log(Number.parseFloat(' 2.5rem'))  
+
+//// They are global function so, the syntax below works as well - OldSchool way
+// console.log(parseFloat(' 2.5rem'))  
+
+//// isNaN - Check if value is not a number
+
+// console.log(Number.isNaN(20))
+// console.log(Number.isNaN('20'))
+// console.log(Number.isNaN(+'20x')) // Not a number , true
+
+//// Checking if value is a Number
+// console.log(Number.isFinite(20))
+// console.log(Number.isFinite('20'))
+// console.log(Number.isFinite(+'20x'))
+// console.log(Number.isFinite(23 / 0))
+
+// console.log(Number.isInteger(23))
+// console.log(Number.isInteger(23.0))
+// console.log(Number.isInteger(23 / 0)) // This return Infinity so not a number
+
+//// IMPORTANT The ParsFloat should be the go to, to read values coming out of CSS
+// console.log(Number.parseFloat(' 2.5rem'))  
+
+
+//// Generating min max number
+
+// const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1) 
+// console.log(randomInt(1, 5))
+
+
+//// Rounding Integers
+
+// console.log(Math.trunc(23.3))
+
+// console.log(Math.round(26.5))
+// console.log(Math.round(26.4))
+
+// console.log(Math.trunc(-23.3)) // Trunc just cuts the decimal
+// console.log(Math.floor(-23.3)) // Rounds down to the right number value even if its negative
+
+//// Rounding Decimals 
+
+// toFixed always returns a string and not a number IMPORTANT
+// console.log((2.7).toFixed(0))
+// console.log((2.7).toFixed(3))
+// console.log(+(2.345).toFixed(2))
+
+// const isEven = n => n % 2 === 0
+
+// console.log(isEven(23))
+// console.log(isEven(20))
+// console.log(isEven(522))
+
+//// If need to do every nth time is a good idea to use the reminder!
+
+// labelBalance.addEventListener('click', ()=>{
+
+//   [...document.querySelectorAll('.movements__row')].forEach((row, i) =>{ 
+//     // 0, 2, 4, 6
+//     if(i % 2 === 0){
+//       row.style.backgroundColor = 'gray'
+//     }
+//     // 0, 3, 6, 9
+//     if(i % 3 === 0){
+//       row.style.backgroundColor = 'lightblue'
+//     }
+//   })
+// })
+
+
+// console.log(Number('23.5'))
+// console.log(Number('23.555.00'))
+
+
+// // BigInt
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(typeof 90071992547409915n, 90071992547409915n)
+// console.log(1000000000000000000000000000000000000000000n)
+
+// console.log(10n / 3n) // bitInts cuts decimal on division 
+// console.log(10 / 3)
+
+//// Creating a date
+
+// const now = new Date() 
+// console.log(now)
+
+// console.log(new Date('Sat Jul 01 2023 14:35:07'))
+// console.log(new Date('Dec 24, 2023'))
+
+// console.log(account1.movementsDates[0])
+
+//  const future = new Date(2037, 10, 19, 15, 23)
+//  console.log(+future)
+
+//  const calcdaysPassed = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 60 * 24)
+
+
+//  const days1 = calcdaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 4)) 
+//  console.log(days1)
+
+
+
+
+
+// const num = 3888764.23
+
+// const options = {
+//   style: 'currency',
+//   unit: 'celsius', 
+//   currency: 'BRL',
+//   // useGrouping: true
+// }
+
+// console.log('US:' , new Intl.NumberFormat('en-US', options).format(num))
+// console.log('BR:', new Intl.NumberFormat('pt-BR', options).format(num))
+// console.log('GER:', new Intl.NumberFormat('de-DE', options).format(num))
