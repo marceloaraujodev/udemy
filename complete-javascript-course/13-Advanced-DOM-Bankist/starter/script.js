c6141611e6ddcabc9cc6a1cae5ce4729e367d5e3
codetrsh <134286416+codetrsh@users.noreply.github.com>
2023-07-06 16:46:14 -0300

    aft

--- complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course/13-Advanced-DOM-Bankist/starter/script.js
@@ -155,16 +155,11 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 headerObserver.observe(header)
 
 
-
-
-
-
 //// Reveal section--hidden using Intersection observer
 const allSections = document.querySelectorAll('.section') // returns a node list
 
 const revealSection = function(entries, observer){
   const [entry] = entries;
-  console.log(entry)
 
   if(!entry.isIntersecting) return
   entry.target.classList.remove('section--hidden')
@@ -178,31 +173,207 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function(section) {
   sectionObserver.observe(section)
-  section.classList.add('section--hidden')
+  // section.classList.add('section--hidden')
 })
 
 
 
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]')
+
+const loadImg = function (entries, observer){
+  const [entry] = entries
+
+  if (!entry.isIntersecting) return; // if not intersecting return (faz nada)
+
+  // replace image
+  entry.target.src = entry.target.dataset.src // replcing the img all data properties are stored at dataset
+
+  // remove the filter class
+  entry.target.addEventListener('load', (e) => { // listen to load event, happens behind scenes
+    console.log(e.target, 'loaded') 
+    e.target.classList.remove('lazy-img') // e.target is the image
+  })
+
+  observer.unobserve(entry.target) // stop observing after images are loaded
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,  // null = the entire viewport
+  threshold: 0,
+  rootMargin: '200px'
+})
+
+imgTargets.forEach(img => imgObserver.observe(img))
+
+
+// Slide Component
+const slider = function (){
+  const slides = document.querySelectorAll('.slide')
+  const btnLeft = document.querySelector('.slider__btn--left')
+  const btnRight = document.querySelector('.slider__btn--right')
+  const dotContainer = document.querySelector('.dots')
+  
+  let curSlide = 0
+  let maxSlide = slides.length
+  
+            // Funtions
+  // Creating Dots
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML('beforeend', `<button class="dots__dot" data-slide="${i}"></button>`)
+    })
+  }
+  
+  // Activating the dot
+  const activateDot = function(slide){
+    document.querySelectorAll('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'))
+    document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active')
+  }
+  
+  
+  const gotToSlide = function (slide) {
+    slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  }
+  
+  // Next slide
+  const nextSlide = function () {
+    
+    if(curSlide === maxSlide -1){
+      curSlide = 0
+    }else{
+      curSlide++
+    }
+  
+    gotToSlide(curSlide)
+    activateDot(curSlide)
+  }
+  
+  // Previous slide
+  const previousSlide = function () {
+    if(curSlide === 0){
+      curSlide = (maxSlide - 1)
+      console.log(curSlide)
+      console.log(maxSlide)
+    }else{
+      curSlide--
+    }
+    gotToSlide(curSlide)
+    activateDot(curSlide)
+  }
+  
+  // Initializing - When page loads
+  const init = function (){
+    createDots()
+    activateDot(0)
+    gotToSlide(0) // starts the app on the first slide
+    gotToSlide(0) // starts the app on the first slide
+  }
+  init()
+  
+  
+  // Event Handlers
+  btnRight.addEventListener('click', nextSlide)
+  btnLeft.addEventListener('click', previousSlide)
+  
+  document.addEventListener('keydown', function (e) {
+    if(e.key === 'ArrowRight') nextSlide()
+    if(e.key === 'ArrowLeft') previousSlide()
+    else return
+  })
+  
+  // Dots functionality
+  dotContainer.addEventListener('click', (e) =>{
+    
+    if(e.target.classList.contains('dots__dot')){
+      const {slide} = e.target.dataset
+      gotToSlide(slide)
+      activateDot(slide)
+    }
+  })
+
+}
+slider()
+
+
+document.addEventListener('DOMContentLoaded', function (e){
+console.log('html parsed and dom tree built', e)
+})
+
+
+
+
+
+/*                                     TITLE  Logic for the slider 
+curSlide = 0; -100%, 0%, 200%; 
+100 * (i - curSlide)
+first interation , index = 0 and curslide = 0. result =  0
+second           , index = 1 and curslide = 1. result = -1
+third            , index = 2 and curslide = 2. result = -2
+
+formula 100 (i - curSlide) = 0
+formula 100 (i - curSlide) = -100
+formula 100 (i - curSlide) = -200
+ */
+
+
+
+/* TITLE SLIDE COMPONENT
+STRATEGY:
+1. Slides move side to side.
+2. When cliked next slide, hide one slide, display next slide using classes
+3. Move the slides with js transform: translateX 0% visible slide, -100 to left, + 100 to right
+4. Formula for the slide calc is 100 * the index, 0 * 100 = 0%, 100 * 1 = 100% ...
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                          
+
+/*                              TITLE LAZY LOADING IMAGES  Intersection observer 
+THE STRATEGY: 
+
+1. Load a very small res image to the page. 
+2. Create a class with a filter ( filter: blur(20px) ) for the blur so you dont see pixelations 
+3. Use data-src="" to reference the image, then replace the images with javascript
+4. The removing of the class that contains the blur filter. Its a bit tricky because the loading of the high res image happens behind the scenes with the load event and when its finished it will ommit the load event! So just listen to the load event on the image.
+5. Use the rootMargin '200px' to load the images 200px before the user reaches the image.
+
+*/
+
+
 /* 
-//                        TITLE   Intersection observer  OBSERVE() EXPLANATION
+                             TITLE   Intersection observer  OBSERVE() EXPLANATION
 
-// const obsCallBack = function (entries, observer) {
-  //   entries.forEach(entry => console.log(entry))
-  // }
+const obsCallBack = function (entries, observer) {
+    entries.forEach(entry => console.log(entry))
+  }
   
-  // const obsOptions = {
-  //   root: null, // When null it will be the entire viewport
-  //   threshold: [0, 0.2]
-  // }
+  const obsOptions = {
+    root: null, // When null it will be the entire viewport
+    threshold: [0, 0.2]
+  }
   
-  // const observer = new IntersectionObserver(obsCallBack, obsOptions)
-  // observer.observe(section1)
+  const observer = new IntersectionObserver(obsCallBack, obsOptions)
+  observer.observe(section1)
 
 
-//                              .observe()  IMPORTANT
-// The call back function obsCallBack will be call each time that observe element in this case 'section1' is intercecting the root element in this case the 'viewport' at the threshold we defined.
+                                .observe()  IMPORTANT
 
-// Setting the root to null will be only looking for when the 'section1' enters the viewpor for more than 10% or 0.1! Based on a threshold: 0.1
+ The call back function obsCallBack will be call each time that observe element in this case 'section1' is intercecting the root element in this case the 'viewport' at the threshold we defined.
+ Setting the root to null will be only looking for when the 'section1' enters the viewpor for more than 10% or 0.1! Based on a threshold: 0.1
+
 
                                             IMPORTANT
 The rootMargin Will activate the threshold exactly where the target area start minus the size of the NAV bar, instead of overlapping.
@@ -212,6 +383,8 @@ rootMargin: '-90px' it will activate the Sticky Nav bar whenever it reaches 90 p
 obs: It has to be pixels! % wont work neither will rem
 
 
+                                            IMPORTANT
+Look at the target it will tell where the intersecting starts happening.
 
 
  Sticky Navigation Bar ---  Not really good, very performance heavy ------------------
@@ -258,18 +431,6 @@ nav.addEventListener('mouseout', function(e){
 */
 
 //// -------END OF - Menu Fade Animation - Passing 'Arguments' to Event Handlers----------------------
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
